f6e5b6e079874de34588b22071611c065e30db62
Salvatore Cirillo <salvatore.cirillo@gmail.com>
2021-06-22 12:26:30 +0100

    Added write callback function

--- login.c
+++ login.c
@@ -36,14 +36,103 @@ static const char* client_id = "enterprise-app";
 static const char* client_secret = "dde2bea7-29b9-492a-aaae-5450b0e72d53";
 
 
+/* holder for curl fetch */
+struct curl_fetch_st {
+    char* payload;
+    size_t size;
+};
+
+/* callback for curl fetch */
+size_t curl_callback(void* contents, size_t size, size_t nmemb, void* userp) {
+    size_t realsize = size * nmemb;                             /* calculate buffer size */
+    struct curl_fetch_st* p = (struct curl_fetch_st*)userp;   /* cast pointer to fetch struct */
+
+    /* expand buffer using a temporary pointer to avoid memory leaks */
+    char* temp = realloc(p->payload, p->size + realsize + 1);
+
+    /* check allocation */
+    if (temp == NULL) {
+        /* this isn't good */
+        fprintf(stderr, "ERROR: Failed to expand buffer in curl_callback");
+        /* free buffer */
+        free(p->payload);
+        /* return */
+        return 1;
+    }
+
+    /* assign payload */
+    p->payload = temp;
+
+    /* copy contents to buffer */
+    memcpy(&(p->payload[p->size]), contents, realsize);
+
+    /* set new buffer size */
+    p->size += realsize;
+
+    /* ensure null termination */
+    p->payload[p->size] = 0;
+
+    /* return size */
+    return realsize;
+}
+
+/* fetch and return url body via curl */
+CURLcode curl_fetch_url(CURL* ch, const char* url, struct curl_fetch_st* fetch) {
+    CURLcode rcode;                   /* curl result code */
+
+    /* init payload */
+    fetch->payload = (char*)calloc(1, sizeof(fetch->payload));
+
+    /* check payload */
+    if (fetch->payload == NULL) {
+        /* log error */
+        fprintf(stderr, "ERROR: Failed to allocate payload in curl_fetch_url");
+        /* return error */
+        return CURLE_FAILED_INIT;
+    }
+
+    /* init size */
+    fetch->size = 0;
+
+    /* set url to fetch */
+    curl_easy_setopt(ch, CURLOPT_URL, url);
+
+    /* set calback function */
+    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_callback);
+
+    /* pass fetch struct pointer */
+    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void*)fetch);
+
+    /* set default user agent */
+    curl_easy_setopt(ch, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    /* set timeout */
+    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 15);
+
+    /* enable location redirects */
+    curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    /* set maximum allowed redirects */
+    curl_easy_setopt(ch, CURLOPT_MAXREDIRS, 1);
+
+    /* fetch the url */
+    rcode = curl_easy_perform(ch);
+
+    /* return */
+    return rcode;
+}
+
+
 bool Login(char* username, char* password) {
     CURL* curl;
     CURLcode res;
 
     struct curl_slist* headers = NULL;                      /* http headers to send with request */
 
-
     bool ret = false;
+
+    struct curl_fetch_st curl_fetch;                        /* curl fetch struct */
+    struct curl_fetch_st* cf = &curl_fetch;                 /* pointer to fetch struct */
 
 
     /* set content type */
@@ -60,34 +149,36 @@ bool Login(char* username, char* password) {
     if (post_fields != NULL) {
         sprintf(post_fields, "grant_type=%s&client_id=%s&client_secret=%s&username=%s&password=%s", grant_type, client_id, client_secret, username, password);
     } else {
-        sprintf(stderr, "Menmory alloc failed !\n");
+        sprintf(stderr, "Memory alloc failed !\n");
         ret = false;
     }
 
-    //curl_global_init(CURL_GLOBAL_DEFAULT);
+
+
+    /* In windows, this will init the winsock stuff */
+    //res = curl_global_init(CURL_GLOBAL_DEFAULT);
+    /* Check for errors */
+    /*
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_global_init() failed: %s\n",
+            curl_easy_strerror(res));
+        return 1;
+    }*/
 
     curl = curl_easy_init();
 
     if (curl) {
-        //curl_easy_setopt(curl, CURLOPT_POST, 1L);
-
-        /* set default user agent */
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-        /* set timeout */
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15);
-
-        /* enable location redirects */
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-
-        /* set maximum allowed redirects */
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 1);
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
 
 
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+
+        //curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.2.25:8080/auth/realms/sypnos/protocol/openid-connect/token");
+        /* set url to fetch */
+        //curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.2.25:8080/auth/realms/sypnos/protocol/openid-connect/token");
+        char* url = "http://192.168.2.25:8080/auth/realms/sypnos/protocol/openid-connect/token";
+
         /* Now specify the POST data */
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
 
@@ -117,9 +208,15 @@ bool Login(char* username, char* password) {
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 #endif
 
+        /* fetch page and capture return code */
+        res = curl_fetch_url(curl, url, cf);
+
         /* Esegue tutte le istruzioni che abbiam dati fin ora, res conterra
          * il codice d'errore */
-        res = curl_easy_perform(curl);
+        //res = curl_easy_perform(curl);
+
+
+
         if (res == CURLE_OK) {
             long http_code = 0;
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
@@ -131,15 +228,38 @@ bool Login(char* username, char* password) {
             ret = false;
         }
 
-        free(post_fields);
+        
 
-        /* free headers */
-        curl_slist_free_all(headers);
-
-        /* Pulisce il nostro handle  */
+        /* cleanup curl handle */
         curl_easy_cleanup(curl);
+        
+    }
 
-        //curl_global_cleanup();
+
+    /* free post fields */
+    free(post_fields);
+
+    /* free headers */
+    curl_slist_free_all(headers);
+
+    //curl_global_cleanup();
+
+
+     /* check payload */
+    if (cf->payload != NULL) {
+        /* print result */
+        printf("CURL Returned: \n%s\n", cf->payload);
+        /* parse return */
+        //json = json_tokener_parse_verbose(cf->payload, &jerr);
+        /* free payload */
+        free(cf->payload);
+    } else {
+        /* error */
+        fprintf(stderr, "ERROR: Failed to populate payload");
+        /* free payload */
+        free(cf->payload);
+        /* return */
+        return 1;
     }
 
     return ret;
